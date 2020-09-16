9d2edbb0cac31dcfce3cb02606f533443c222d5d
Klachkov Valery <0gdump.kv@gmail.com>
2020-09-17 07:10:24 +1000

    [ui/main] Implement navigation handle

--- app/src/main/java/open/zgdump/simplefinance/ui/main/MainFlow.kt
+++ app/src/main/java/open/zgdump/simplefinance/ui/main/MainFlow.kt
@@ -1,6 +1,8 @@
 package open.zgdump.simplefinance.ui.main
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main_container.*
@@ -20,6 +22,7 @@ class MainFlow : MvpFragmentX(R.layout.fragment_main) {
         super.onActivityCreated(savedInstanceState)
 
         navigationView.setNavigationItemSelectedListener {
+            navigateTo(it)
             drawerLayout.closeDrawers()
             true
         }
@@ -48,6 +51,49 @@ class MainFlow : MvpFragmentX(R.layout.fragment_main) {
         ).apply {
             drawerLayout.setDrawerListener(this)
             syncState()
+        }
+    }
+
+    private fun navigateTo(menuItem: MenuItem) {
+        if (menuItem.itemId == navigationView.checkedItem?.itemId) return
+        when (menuItem.itemId) {
+            R.id.navHome -> {
+                Log.d("TEST", "navHome")
+            }
+            R.id.navAccounts -> {
+                Log.d("TEST", "navAccounts")
+            }
+            R.id.navBudget -> {
+                Log.d("TEST", "navBudget")
+            }
+            R.id.navGroupMoney -> {
+                Log.d("TEST", "navGroupMoney")
+            }
+            R.id.navIncome -> {
+                Log.d("TEST", "navIncome")
+            }
+            R.id.navExpense -> {
+                Log.d("TEST", "navExpense")
+            }
+            R.id.navLoan -> {
+                Log.d("TEST", "navLoan")
+            }
+            R.id.navSms -> {
+                Log.d("TEST", "navSms")
+            }
+            R.id.navChart -> {
+                Log.d("TEST", "navChart")
+            }
+            R.id.navCurrencies -> {
+                Log.d("TEST", "navCurrencies")
+            }
+            R.id.navCategories -> {
+                Log.d("TEST", "navCategories")
+            }
+            R.id.navMore -> {
+                Log.d("TEST", "navMore")
+            }
+            else -> throw IllegalArgumentException()
         }
     }
 }
