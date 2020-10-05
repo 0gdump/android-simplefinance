71b73edb473ebe1961de277bb33ba86f4c166dc8
Valery 0gdump Klachkov <0gdump.kv@gmail.com>
2020-10-05 14:43:01 +1000

    [repository/dao] Rewrite CurrencyDao Remove unused method, use indexes and offsets instead of id's

--- app/src/main/java/open/zgdump/simplefinance/repository/dao/CurrencyDao.kt
+++ app/src/main/java/open/zgdump/simplefinance/repository/dao/CurrencyDao.kt
@@ -5,26 +5,53 @@ import open.zgdump.simplefinance.entity.Currency
 import open.zgdump.simplefinance.global.RoomTablesNames.CURRENCIES_TABLE_NAME
 
 @Dao
-interface CurrencyDao {
+abstract class CurrencyDao {
 
-    @Query("SELECT * FROM $CURRENCIES_TABLE_NAME")
-    suspend fun getAll(): List<Currency>?
+    @Query(
+        """
+        SELECT *
+        FROM $CURRENCIES_TABLE_NAME
+        WHERE rowid in (
+            SELECT rowid 
+            FROM $CURRENCIES_TABLE_NAME
+            LIMIT :count
+            OFFSET :offset
+        )
+    """
+    )
+    abstract suspend fun getCurrencies(offset: Int, count: Int): List<Currency>?
 
-    @Query("SELECT * FROM $CURRENCIES_TABLE_NAME WHERE id >= :id LIMIT :count")
-    suspend fun getCurrenciesFromId(id: Int, count: Int): List<Currency>?
-
-    @Query("SELECT * FROM $CURRENCIES_TABLE_NAME WHERE id = :id")
-    suspend fun getById(id: Int): Currency?
-
-    @Query("SELECT * FROM $CURRENCIES_TABLE_NAME WHERE name = :k OR designation = :k")
-    suspend fun getByCriteria(k: String): Currency?
+    @Query(
+        """
+        SELECT *
+        FROM $CURRENCIES_TABLE_NAME
+        WHERE rowid = (
+            SELECT rowid 
+            FROM $CURRENCIES_TABLE_NAME
+            LIMIT 1
+            OFFSET :index
+        )
+    """
+    )
+    abstract suspend fun getCurrency(index: Int): Currency?
 
     @Insert
-    suspend fun insert(currency: Currency)
+    abstract suspend fun insert(currency: Currency)
 
     @Update
-    suspend fun update(currency: Currency)
+    abstract suspend fun update(currency: Currency)
 
-    @Delete
-    suspend fun delete(currency: Currency)
+    @Query(
+        """
+        DELETE 
+        FROM $CURRENCIES_TABLE_NAME
+        WHERE rowid = (
+            SELECT rowid 
+            FROM $CURRENCIES_TABLE_NAME
+            LIMIT 1 
+            OFFSET :index
+        )
+    """
+    )
+    abstract fun delete(index: Int)
 }
